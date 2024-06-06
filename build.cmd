f61621d6ef405674f323912e8d33a9698905218a
Aruvelut-123 <2502474438@qq.com>
2024-06-07 00:04:01 +0800

    nothing changes, bug fixed

--- build.cmd
+++ build.cmd
@@ -1,5 +1,8 @@
 cd /d %cd%
+$env:ELECTRON_MIRROR="https://idk.minecraftisbest.top/"
 npm run-script package
 npm run-script packagelinux
 npm run-script packagemac
+npm run-script zip
+npm run-script ziplinux
 npm run-script zipmac
