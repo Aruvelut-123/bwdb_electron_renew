9e24f427be3140d5f805386e0254ea11c7b137bc
Aruvelut <2502474438@qq.com>
2024-06-20 17:31:06 +0800

    1.0.1 update with broken database updater

--- main.js
+++ main.js
@@ -375,6 +375,23 @@ function createHashSettingWindow() {
   flaghash = 0;
   //hashwindow.webContents.openDevTools();
 }
+function createUpdateDatabaseWindow() {
+  if (os.platform != "darwin") {
+    Menu.setApplicationMenu(null);
+  }
+  updatewindow = new BrowserWindow({
+    width: 650,
+    height: 400,
+    resizable: false,
+    maximizable: false,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  updatewindow.loadFile('update.html');
+  //updatewindow.webContents.openDevTools();
+}
 // Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
 // 部分 API 在 ready 事件触发后才能使用。
 function app_ready_do() {
@@ -441,6 +458,10 @@ ipcMain.on('show-win', (event, args) => {
       hashwindow.webContents.send("setmode", 1);
     }
     hashwindow.show();
+    return 0;
+  }
+  if (args == "update") {
+    updatewindow.show();
     return 0;
   }
 });
@@ -814,6 +835,9 @@ ipcMain.on('openhashwindow', (event, args) => {
     flaghash = 1;
   }
 });
+ipcMain.on('openupdatewindow', (event, args) => {
+  createUpdateDatabaseWindow();
+});
 ipcMain.on('closeabout', (event, args) => {
   aboutWindow.close();
 });
@@ -1129,6 +1153,28 @@ ipcMain.on('msgfoward', (event, args) => {
   }
 });
 ipcMain.on('getsha256list', (event, args) => {
+  workerwindow = new BrowserWindow({
+    minHeight: 300,
+    minWidth: 600,
+    width: 900,
+    height: 600,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      directWrite: false
+    }
+  });
+  if (!app.isPackaged) {
+    workerwindow.webContents.openDevTools();
+    workerwindow.show();
+  }
+  // 并且为你的应用加载index.html
+  workerwindow.loadFile('worker.html');
+  workerwindow.on("close", function () {
+    workerwindow = null;
+  });
+});
+ipcMain.on('update', (event, args) => {
   workerwindow = new BrowserWindow({
     minHeight: 300,
     minWidth: 600,
